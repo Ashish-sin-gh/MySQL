44fad84162e026c57a5bc6b75eff739e7b465972
Ashish Kumar Singh <ashish159singh@gmail.com>
2024-08-03 11:09:06 +0530

    view query added

--- VIEW.sql
+++ VIEW.sql
@@ -1,2 +1,10 @@
 USE org;
 
+select * from worker;
+
+-- create a view table from emp table 
+CREATE VIEW worker_view 
+AS SELECT first_name, salary FROM worker; 
+
+-- diplay a view 
+SELECT * FROM  worker_view;
